218c1c9f57f32667db423af0688b628ea2613610
Andrei Voinea <andreivdev@gmail.com>
2020-11-01 22:43:32 +0200

    single player, multi player

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const Player = (_sign, name) => {
+const Player = (_sign, _name) => {
+  let name = _name;
   let sign = _sign;
   let wins = 0;
   const saySign = () => {
     return sign;
+  };
+
+  const sayName = () => {
+    return name;
   };
 
   const isWinner = () => {
@@ -10,13 +15,98 @@ const Player = (_sign, name) => {
     wins++;
   };
 
-  return { saySign, isWinner };
+  const getWins = () => {
+    return wins;
+  };
+
+  return { saySign, sayName, isWinner, getWins };
 };
 
 const game = (() => {
   let currentPlayer;
+  let gameMode;
+
+  const start = () => {
+    let chooseGameMode = document.querySelector(".chooseGameMode");
+    chooseGameMode.style.display = "grid";
+
+    let singlePlayerButton = document.querySelector(".singleplayer");
+    let multiPlayerButton = document.querySelector(".multiplayer");
+    singlePlayerButton.addEventListener("click", function () {
+      game.setGameMode("singleplayer");
+      startSinglePlayer();
+    });
+
+    multiPlayerButton.addEventListener("click", function () {
+      game.setGameMode("multiplayer");
+      startMultiPlayer();
+    });
+  };
+
+  const startSinglePlayer = () => {
+    let chooseGameMode = document.querySelector(".chooseGameMode");
+    chooseGameMode.style.display = "none";
+
+    let multiplayerscreen = document.querySelector(".singleplayerscreen");
+    multiplayerscreen.style.display = "grid";
+
+    let startMultiplayerButton = document.querySelector(".startsingleplayer");
+    startMultiplayerButton.addEventListener("click", function () {
+      let player1Name = (Name = document.querySelector(".player1"));
+      if (player1Name.value != "") {
+        multiplayerscreen.style.display = "none";
+
+        playerOne = Player("X", player1Name.value);
+        playerTwo = Player("O", "Computer");
+
+        game.setCurrentPlayer(playerOne);
+        game.play();
+        drawScoreBoard();
+      }
+    });
+  };
+
+  const startMultiPlayer = () => {
+    let chooseGameMode = document.querySelector(".chooseGameMode");
+    chooseGameMode.style.display = "none";
+
+    let multiplayerscreen = document.querySelector(".multiplayerscreen");
+    multiplayerscreen.style.display = "grid";
+
+    let startMultiplayerButton = document.querySelector(".startmultiplayer");
+    startMultiplayerButton.addEventListener("click", function () {
+      let player1Name = document.querySelector(".player1");
+      let player2Name = document.querySelector(".player2");
+
+      if (player1Name.value != "" && player2Name.value != "") {
+        multiplayerscreen.style.display = "none";
+
+        playerOne = Player("X", player1Name.value);
+        playerTwo = Player("O", player2Name.value);
+
+        game.setCurrentPlayer(playerOne);
+        game.play();
+        drawScoreBoard();
+      } else alert("Please choose player names");
+    });
+  };
+
+  const drawScoreBoard = () => {
+    let player1Score = document.querySelector(".player1score");
+    let player2Score = document.querySelector(".player2score");
+
+    player1Score.innerHTML =
+      playerOne.sayName() + ": " + playerOne.getWins() + " wins";
+
+    player2Score.innerHTML =
+      playerTwo.sayName() + ": " + playerTwo.getWins() + " wins";
+  };
+  const setGameMode = (mode) => {
+    gameMode = mode;
+  };
 
   const play = () => {
+    drawScoreBoard();
     gameboard.resetBoard();
     gameboard.draw();
   };
@@ -35,10 +125,24 @@ const game = (() => {
     } else {
       alert("Please pick a valid block");
     }
+
     if (currentPlayer == playerOne) currentPlayer = playerTwo;
     else currentPlayer = playerOne;
+
+    if (gameMode == "singleplayer" && currentPlayer == playerTwo) {
+      //computer
+      playComputer();
+    }
   };
-  return { setCurrentPlayer, click, play };
+
+  const playComputer = () => {
+    let moves = gameboard.possibleMoves();
+    console.log(moves);
+    let move = moves[Math.floor(parseFloat(moves.length) * Math.random())];
+    console.log(move);
+    click(move);
+  };
+  return { start, setGameMode, setCurrentPlayer, click, play };
 })();
 
 const gameboard = (() => {
@@ -107,11 +211,31 @@ const gameboard = (() => {
     } else return false;
   };
 
-  return { draw, mark, resetBoard, checkGameOver };
+  const possibleMoves = () => {
+    let possibleMoves = [];
+    for (i = 0; i < gridData.length; i++) {
+      if (gridData[i] != "X" && gridData[i] != "O") {
+        possibleMoves.push(i);
+      }
+    }
+    return possibleMoves;
+  };
+  return { draw, mark, resetBoard, checkGameOver, possibleMoves };
 })();
 
-const playerOne = Player("X", "The X");
+/* const playerOne = Player("X", "The X");
 const playerTwo = Player("O", "The O");
 
 game.setCurrentPlayer(playerOne);
 game.play();
+ */
+
+//choose game mode
+
+let playerOne, playerTwo;
+
+document.querySelector(".chooseGameMode").style.display = "none";
+document.querySelector(".singleplayerscreen").style.display = "none";
+document.querySelector(".multiplayerscreen").style.display = "none";
+
+game.start();
